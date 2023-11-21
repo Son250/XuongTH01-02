d474e9aa46bcf216bb8875b8205f80af9b4e5043
Nguyen Viet Son <anhsongoku123@gmail.com>
2023-11-21 17:54:52 +0700

    XuongTH cap nhat ngay 21/11 5:54 PM

--- WebThiTracNghiem/view/_header.php
+++ WebThiTracNghiem/view/_header.php
@@ -10,7 +10,10 @@
     <meta content="" name="keywords">
 
     <!-- Đường link cũ  -->
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style-view.css">
+    <link rel="stylesheet" href="../assets/css/style_trangchu.css">
+    <link rel="stylesheet" href="../assets/css/style_admin.css">
+
     <link href="../assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
